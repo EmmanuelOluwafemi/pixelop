975cf37fd8f185ea9329e084c4e97054cfc59b8c
Emmanuel Yusuf <seek9ja@gmail.com>
2021-03-26 20:12:16 +0100

    remove unused package call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Routes from './route/Routes';
 import 'bootstrap/dist/css/bootstrap.css';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 import "./utilities/font/stylesheet.css";
 
 import "aos/dist/aos.css";
